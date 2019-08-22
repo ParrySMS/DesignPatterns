43e0f71455fdb333ab51967eaf71faffde1c1544
ParrySMS <yh@szu.me>
2019-08-23 06:52:43 +0800

    finish Temp hook

--- README.md
+++ README.md
@@ -1126,4 +1126,128 @@ $clinet = new TMClient();
 	- 尽管子类可以改变钩子的行为，但仍要遵循定义的模板方法顺序
 	- 某个钩子方法, 用一些条件来控制具体执行内容。
 	- 在所有的钩子操作中，必须要警告控制流，说明当前状态有不同情况发生，不是执行默认的控制流。
+	- 一个购物清单的例子 [TemplateHook.php](https://github.com/ParrySMS/DesignPatterns/blob/master/TemplateHook.php)
+	
+```php
+<?php
+/**
+ * Class IShopHook
+ *  add tax and shipping, then show the cost
+ */
+abstract class IShopHook
+{
+    protected $purchased;
+    protected $discount_hook;
+    protected $shipping_hook;
+    protected $total;
+
+    // define the sequence
+    public function templateMethod($purchased, $hasDiscount, $hasShipping)
+    {
+        $this->purchased = $purchased;
+        $this->discount_hook = $hasDiscount;
+        $this->shipping_hook = $hasShipping;
+        $this->addTax();
+        $this->addDiscountHook();
+        $this->addShippingHook();
+        $this->displayCost();
+    }
+    protected abstract function addTax();
+    protected abstract function addDiscountHook();
+    protected abstract function addShippingHook();
+    protected abstract function displayCost();
+}
+
+/**
+ * class JingDongShop
+ */
+class JingDongShop extends IShopHook
+{
+    const RADIO_TAX_JD = 0.05;
+    const RADIO_DISCOUNT_OFF = 0.20;
+    const SHIPPING_FEE = 9;
+
+    protected function addTax()
+    {
+        $this->total = (1 + $this::RADIO_TAX_JD) * $this->purchased;
+        echo "after tax: $this->total" . PHP_EOL;
+    }
+
+    protected function addDiscountHook()
+    {
+        if ($this->discount_hook) {
+            $this->total = intval((1 - $this::RADIO_DISCOUNT_OFF) * $this->total);
+            echo "after discount: $this->total" . PHP_EOL;
+        }
+    }
+
+    protected function addShippingHook()
+    {
+        if ($this->shipping_hook) {
+            $this->total += $this::SHIPPING_FEE;
+            echo "after shipping: $this->total" . PHP_EOL;
+        }
+    }
+
+    protected function displayCost()
+    {
+        echo "final: $this->total" . PHP_EOL;
+    }
+}
+
+/**
+ *  20% off if the price more than 999 RMB
+ *  need shipping fee if price less than 99 RMB
+ */
+class Client
+{
+    const DEFAULT_PRODUCTS = ['Dell-laptop' => 6799, 'IPad2019' => 7859, 'IPhone-XR' => 5100];
+    private $product;
+    private $shop;
+    private $cost;
+    private $hasDiscount;
+    private $hasShipping;
+
+    public function __construct(IShopHook $shop, $product = null)
+    {
+
+        $this->setProduct($product);
+        $this->setCost();
+        $this->shop = $shop;
+        $this->hasDiscount = ($this->cost > 999);
+        $this->hasShipping = ($this->cost < 99);
+        $this->shop->templateMethod($this->cost, $this->hasDiscount, $this->hasShipping);
+    }
+
+    private function setProduct($product)
+    {
+        if ($product === null) {
+            $this->product = $this::DEFAULT_PRODUCTS;
+        } else {
+            $this->product = $product;
+        }
+    }
+
+    private function setCost()
+    {
+        echo 'Products :'.PHP_EOL;
+        foreach ($this->product as $name =>$price) {
+            echo "$name : $price RMB".PHP_EOL;
+            $this->cost += $price;
+        }
+        echo PHP_EOL;
+    }
+}
+
+$worker = new Client(new JingDongShop());
+//Products :
+//Dell-laptop : 6799 RMB
+//IPad2019 : 7859 RMB
+//IPhone-XR : 5100 RMB
+//
+//after tax: 20745.9
+//after discount: 16596
+//final: 16596
+```
+	
 
--- TemplateHook.php
+++ TemplateHook.php
@@ -12,20 +12,20 @@ abstract class IShopHook
     protected $total;
 
     // define the sequence
-    public function templateMethod($purchased, $hasDiscount,$hasShipping)
+    public function templateMethod($purchased, $hasDiscount, $hasShipping)
     {
         $this->purchased = $purchased;
         $this->discount_hook = $hasDiscount;
-		$this->shipping_hook = $hasShipping;
+        $this->shipping_hook = $hasShipping;
         $this->addTax();
-		$this->addDiscountHook();
+        $this->addDiscountHook();
         $this->addShippingHook();
         $this->displayCost();
     }
 
     protected abstract function addTax();
 
-	protected abstract function addDiscountHook();
+    protected abstract function addDiscountHook();
 
     protected abstract function addShippingHook();
 
@@ -38,31 +38,31 @@ abstract class IShopHook
 class JingDongShop extends IShopHook
 {
     const RADIO_TAX_JD = 0.05;
-	const RADIO_DISCOUNT_OFF = 0.20;
+    const RADIO_DISCOUNT_OFF = 0.20;
     const SHIPPING_FEE = 9;
 
     protected function addTax()
     {
-        $this->total = (1 + RADIO_TAX_JD) * $this->purchased;
-		echo "after tax: $this->total" . PHP_EOL;
+        $this->total = (1 + $this::RADIO_TAX_JD) * $this->purchased;
+        echo "after tax: $this->total" . PHP_EOL;
     }
-	
-	protected function addDiscountHook()
+
+    protected function addDiscountHook()
     {
-        if (discount_hook) {
-			$this->total = intval((1 - RADIO_DISCOUNT_OFF)*$this->total);
-			echo "after discount: $this->total" . PHP_EOL;
+        if ($this->discount_hook) {
+            $this->total = intval((1 - $this::RADIO_DISCOUNT_OFF) * $this->total);
+            echo "after discount: $this->total" . PHP_EOL;
         }
     }
 
     protected function addShippingHook()
     {
-        if (shipping_hook) {
-            $this->total += SHIPPING_FEE;
-			echo "after shipping: $this->total" . PHP_EOL;
+        if ($this->shipping_hook) {
+            $this->total += $this::SHIPPING_FEE;
+            echo "after shipping: $this->total" . PHP_EOL;
         }
     }
-	
+
     protected function displayCost()
     {
         echo "final: $this->total" . PHP_EOL;
@@ -75,35 +75,50 @@ class JingDongShop extends IShopHook
  */
 class Client
 {
-	const DEFAULT_PRODUCTSdefault_products = [
-		'Dell-laptop' => 6799,
-		'IPad2019' => 7859,
-		'IPhone-XR' => 5100
-	];
+    const DEFAULT_PRODUCTS = ['Dell-laptop' => 6799, 'IPad2019' => 7859, 'IPhone-XR' => 5100];
     private $product;
-	private $shop;
-	private $cost;
-	private $hasDiscount; 
-	private $hasShipping; 
-	
-    public function __construct(IShopHook $shop,$product = null)
+    private $shop;
+    private $cost;
+    private $hasDiscount;
+    private $hasShipping;
+
+    public function __construct(IShopHook $shop, $product = null)
     {
-		
-		$this->setProduct($product);
-		$this->setCost();
-		$this->shop = $shop;
-		$this->hasDiscount = ($this->cost > 999);
-		$this->hasShipping = ($this->cost < 99); 
-		$this->shop->templateMethod($this->cost,$this->hasDiscount,$this->hasShipping);
+
+        $this->setProduct($product);
+        $this->setCost();
+        $this->shop = $shop;
+        $this->hasDiscount = ($this->cost > 999);
+        $this->hasShipping = ($this->cost < 99);
+        $this->shop->templateMethod($this->cost, $this->hasDiscount, $this->hasShipping);
     }
-	
-	private function setProduct($product)
-	{
-		if($product === null){
-			$product = $this->default_products
-		}	
-	}	
-	
+
+    private function setProduct($product)
+    {
+        if ($product === null) {
+            $this->product = $this::DEFAULT_PRODUCTS;
+        } else {
+            $this->product = $product;
+        }
+    }
+
+    private function setCost()
+    {
+        echo 'Products :'.PHP_EOL;
+        foreach ($this->product as $name =>$price) {
+            echo "$name : $price RMB".PHP_EOL;
+            $this->cost += $price;
+        }
+        echo PHP_EOL;
+    }
 }
 
-
+$worker = new Client(new JingDongShop());
+//Products :
+//Dell-laptop : 6799 RMB
+//IPad2019 : 7859 RMB
+//IPhone-XR : 5100 RMB
+//
+//after tax: 20745.9
+//after discount: 16596
+//final: 16596
